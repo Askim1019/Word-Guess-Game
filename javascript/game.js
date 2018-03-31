b1c018902800c4c5626e5a1b613aff8bd51da248
Alex Kim <askim1019@gmail.com>
2018-03-31 15:13:35 -0500

    Add pseudocode and make skeleeton of the html

---
+++ javascript/game.js
@@ -0,0 +1,14 @@
+// User presses any key to start the game
+
+// Display total guesses left
+
+// User guesses a letter
+
+// If User letter is wrong, display wrong letter on bottom div
+// Subtract from total guesses left
+
+// If user letter is correct, display correct letter in top div
+
+// If user uses up total guesses, display "You Lose"
+
+// If user uses guesses whole world, display "You Win"
