1cc8b717328d0eacbdb22488f1e574d113bc8a0a
Alex Kim <askim1019@gmail.com>
2018-04-01 16:36:05 -0500

    Add css and initial javascript logic

--- javascript/game.js
+++ javascript/game.js
@@ -12,3 +12,33 @@
 // If user uses up total guesses, display "You Lose"
 
 // If user uses guesses whole world, display "You Win"
+
+// Create our word bank or array
+var word = ['vi', 'malphite', 'ekko', 'darius', 'garen', 'ezreal',
+            'leesin', 'khazix', 'rengar', 'karma', 'ashe', 'leona',
+            'lux', 'ahri', 'vayne', 'lucian', 'nocturne', 'renekton',
+            'nasus', 'janna', 'soraka', 'sona', 'draven', 'sivir',
+            'xayah', 'taliyah', 'tryndamere', 'illaoi', 'gragas',
+            'gangplank'];
+
+// Create a variable to get the random index number to choose our word
+var indexOfWord= Math.floor(Math.random() * word.length);
+
+// Get the word from array by using the ranodm function above
+var chooseWord = word[indexOfWord];
+
+// Test to see if word is being chosen and how long the character is
+console.log(chooseWord);
+console.log(chooseWord.length);
+
+var underscores = [];
+
+var makeUnderscore = function () {
+    for (var i = 0; i < chooseWord.length; i++) {
+        underscores.push('_');
+    }
+
+    return underscores;
+};
+
+console.log(makeUnderscore());
