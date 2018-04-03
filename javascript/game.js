855f48cd0031ed103b3e8e6f5a129c42be3def82
Alex Kim <askim1019@gmail.com>
2018-04-03 01:31:26 -0500

    Separate functions, add background and colors, add button

--- javascript/game.js
+++ javascript/game.js
@@ -1,21 +1,25 @@
 // Create our word bank or array
-var wordList = ['vi', 'malphite', 'ekko', 'darius', 'garen', 'ezreal',
-            'leesin', 'khazix', 'rengar', 'karma', 'ashe', 'leona',
-            'lux', 'ahri', 'vayne', 'lucian', 'nocturne', 'renekton',
-            'nasus', 'janna', 'soraka', 'sona', 'draven', 'sivir',
-            'xayah', 'taliyah', 'tryndamere', 'illaoi', 'gragas',
-            'gangplank', 'ziggs'];
+var wordList = ['malphite', 'ekko', 'darius', 'garen', 'ezreal',
+            'leesin', 'khazix', 'rengar','gangplank', 'ziggs'];
+
 
 // Create empty arrays to push to.
 var underscores = [];
 var correctLetters = [];
 var wrongLetters = [];
-var guessesRemaining = 6;
+var guessesRemaining = 10;
+stateOfGame = false;
 
-// Create the logic in a function that we can invoke
-
-function newGame() {
+// start a new game
+function startGame() {
     
+    underscores = [];
+    correctLetters = [];
+    wrongLetters = [];
+    guessesRemaining = 10;
+    stateOfGame = true;
+
+    document.getElementById("instruction").innerHTML = "Guess a letter. You get 10 tries";
     // Create a variable to get the random index number to choose our word
     var indexOfWord= Math.floor(Math.random() * wordList.length);
 
@@ -32,48 +36,63 @@ function newGame() {
     };
 
     makeUnderscore();
-    // Test to see if word is being chosen and how long the character is in console
-    console.log(word);
-    console.log(word.length);
 
-
-    function letterLogic(userLetter, letterIndex) {
+    // If the letter is in the word, display it and replace it with the undescore
+    function correctGuess(userLetter) {
         for (var j = 0; j < word.length; j++) {
             if (word[j] === userLetter) {
-                underscores[j] = word[j];
+                underscores[j] = userLetter;
             }
-            else if (word === underscores.join("")) {
-                document.getElementById("instruction").innerHTML = "YOU WIN!";
-            }
-            else if (guessesRemaining === 0) {
-                document.getElementById("instruction").innerHTML = "YOU LOSE!";
-                break;
-            }
-            else if (letterIndex === -1 && wrongLetters.includes(userLetter) === false) {
-                wrongLetters.push(userLetter);
-                guessesRemaining--;
-            }
+        }
+
+        document.getElementById("underscores").textContent = underscores.join("");
+    }
+
+    // Decrement the total guesses remaining if the letter is not in the word
+    function wrongGuess(userLetter, letterIndex) {
+        if (guessesRemaining > 0 && letterIndex === -1 && !wrongLetters.includes(userLetter)) {
+            wrongLetters.push(userLetter);
+            document.getElementById("wrongLetters").textContent = wrongLetters.join(" ");
+            guessesRemaining--;
+            document.getElementById("guessesRemaining").textContent = guessesRemaining;
         }
     }
 
+    // Type win game and show a picture if you guess all the right letters
+     function winGame() {
+        if (underscores.join("") === word) {
+            document.getElementById("instruction").innerHTML = "You WIN!";
+            stateOfGame = false;
+        }
+        
+    }
+
+    // If you have 0 guesses remaining, stop the game
+    function loseGame() {
+        if (guessesRemaining === 0) {
+            document.getElementById("instruction").innerHTML = "You LOSE!";
+            stateOfGame = false;
+        }  
+    }
+
+    //This is the main key event that renders our guesses
+    
     document.onkeyup =  function(event) {
-        if (event.keyCode >= 65 && event.keyCode <= 90) {
+        if (event.keyCode >= 65 && event.keyCode <= 90 && stateOfGame === true) {
             var userLetter = event.key.toLowerCase(); // letter that is pressed
             var letterIndex = word.indexOf(userLetter);
-            
+            console.log(word);
             console.log(letterIndex);
             console.log(userLetter);
-            //loop through length of word and replace the underscore with a correct letter
-            //If no correct letter then, add to letters incorrect array to render to page.
-            letterLogic(userLetter, letterIndex);
-            
-            // The DOM renders our logic to the page
-            document.getElementById("guessesRemaining").textContent = guessesRemaining;
-            document.getElementById("wrongLetters").textContent = wrongLetters.join(" ");
-            document.getElementById("underscores").textContent = underscores.join("");
+
+            // Call our logical functions
+            correctGuess(userLetter);
+            wrongGuess(userLetter, letterIndex);
+            winGame();
+            loseGame();
+           
         }
     }
-
 }
 
-newGame();
+document.getElementById("btn").addEventListener("click", startGame());
